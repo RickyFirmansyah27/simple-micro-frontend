dfe69539ab8455ca24803792d2917144d26f8dc6
Ricky Firmansyah <ricky.firmansyah@dansmultipro.com>
2023-11-27 13:59:03 +0700

    fix error menu

--- shell_app/src/App.js
+++ shell_app/src/App.js
@@ -1,6 +1,5 @@
-// Import library yang diperlukan
 import React, { lazy, Suspense } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // Import komponen dengan React.lazy
 const Menu = lazy(() => import('feature/Menu'));
@@ -8,12 +7,11 @@ const Feature1 = lazy(() => import('feature/Feature1'));
 const Feature2 = lazy(() => import('feature/Feature2'));
 const Feature3 = lazy(() => import('feature/Feature3'));
 
-// Komponen fallback untuk React.lazy
 const LoadingFallback = () => (
   <div>Loading...</div>
 );
 
-// Komponen utama
+
 const App = () => {
   return (
     <Router>
@@ -22,20 +20,12 @@ const App = () => {
           <h2>Shell App</h2>
         </div>
         <Suspense fallback={<LoadingFallback />}>
-          <Switch>
-            <Route path="/" exact>
-              <Menu />
-            </Route>
-            <Route path="/feature1">
-              <Feature1 />
-            </Route>
-            <Route path="/feature2">
-              <Feature2 />
-            </Route>
-            <Route path="/feature3">
-              <Feature3 />
-            </Route>
-          </Switch>
+          <Routes>
+            <Route path="/" element={<Menu />} />
+            <Route path="/feature1" element={<Feature1 />} />
+            <Route path="/feature2" element={<Feature2 />} />
+            <Route path="/feature3" element={<Feature3 />} />
+          </Routes>
         </Suspense>
       </div>
     </Router>
