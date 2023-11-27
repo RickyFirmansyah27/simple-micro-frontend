e6b0d4708cd4603453d04e83f0d55b2767280f4f
Ricky Firmansyah <ricky.firmansyah@dansmultipro.com>
2023-11-27 13:47:59 +0700

    rollback changes

--- shell_app/src/App.js
+++ shell_app/src/App.js
@@ -1,42 +1,15 @@
-import React, { lazy, Suspense } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React from 'react'
+const Menu = React.lazy(() => import('feature/Feature1'))
 
-const Menu = lazy(() => import('./feature/Menu'));
-const Feature1 = lazy(() => import('./feature/Feature1'));
-const Feature2 = lazy(() => import('./feature/Feature2'));
-const Feature3 = lazy(() => import('./feature/Feature3'));
-
-
-const LoadingFallback = () => (
-  <div>Loading...</div>
-);
-
-const App = () => {
+export default function App() {
   return (
-    <Router>
-      <div>
-        <div style={{ fontSize: '20px' }}>
-          <h2>Shell App</h2>
-        </div>
-        <Suspense fallback={<LoadingFallback />}>
-          <Switch>
-            <Route path="/" exact>
-              <Menu />
-            </Route>
-            <Route path="/feature1">
-              <Feature1 />
-            </Route>
-            <Route path="/feature2">
-              <Feature2 />
-            </Route>
-            <Route path="/feature3">
-              <Feature3 />
-            </Route>
-          </Switch>
-        </Suspense>
+    <div>
+      <div style={{fontSize: '20px'}}>
+        <h2>Shell App</h2>
       </div>
-    </Router>
-  );
+      <div>
+        <Menu />
+      </div>
+    </div>
+  )
 }
-
-export default App;
