5b0eb531181f0e882c1555e4a06f83bbd93a911b
Ricky Firmansyah <ricky.firmansyah@dansmultipro.com>
2023-11-27 13:42:17 +0700

    enhance menu

--- shell_app/src/App.js
+++ shell_app/src/App.js
@@ -1,15 +1,42 @@
-import React from 'react'
-const Menu = React.lazy(() => import('feature/Menu'))
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-export default function App() {
+const Menu = lazy(() => import('./feature/Menu'));
+const Feature1 = lazy(() => import('./feature/Feature1'));
+const Feature2 = lazy(() => import('./feature/Feature2'));
+const Feature3 = lazy(() => import('./feature/Feature3'));
+
+
+const LoadingFallback = () => (
+  <div>Loading...</div>
+);
+
+const App = () => {
   return (
-    <div>
-      <div style={{fontSize: '20px'}}>
-        <h2>Shell App</h2>
-      </div>
+    <Router>
       <div>
-        <Menu />
+        <div style={{ fontSize: '20px' }}>
+          <h2>Shell App</h2>
+        </div>
+        <Suspense fallback={<LoadingFallback />}>
+          <Switch>
+            <Route path="/" exact>
+              <Menu />
+            </Route>
+            <Route path="/feature1">
+              <Feature1 />
+            </Route>
+            <Route path="/feature2">
+              <Feature2 />
+            </Route>
+            <Route path="/feature3">
+              <Feature3 />
+            </Route>
+          </Switch>
+        </Suspense>
       </div>
-    </div>
-  )
+    </Router>
+  );
 }
+
+export default App;
